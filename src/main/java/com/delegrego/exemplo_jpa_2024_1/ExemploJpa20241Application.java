c0023295566319d3b13285bb6eda2601e0027f40
Henrique Delegrego <62546083+HenriqueDelegrego@users.noreply.github.com>
2025-10-01 19:47:17 -0300

    Initial commit

---
+++ src/main/java/com/delegrego/exemplo_jpa_2024_1/ExemploJpa20241Application.java
@@ -0,0 +1,13 @@
+package com.delegrego.exemplo_jpa_2024_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExemploJpa20241Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExemploJpa20241Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/delegrego/exemplo_jpa_2024_1/ExemploJpa20241ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.delegrego.exemplo_jpa_2024_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExemploJpa20241ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
