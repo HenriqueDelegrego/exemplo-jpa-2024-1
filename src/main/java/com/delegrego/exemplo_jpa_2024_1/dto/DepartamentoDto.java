cdd90de10ac3ffd3c9b46c46bf3942172b49635e
Henrique Delegrego <62546083+HenriqueDelegrego@users.noreply.github.com>
2025-10-06 20:51:42 -0300

    cria os DTOs

---
+++ src/main/java/com/delegrego/exemplo_jpa_2024_1/dto/DepartamentoDto.java
@@ -0,0 +1,35 @@
+package com.delegrego.exemplo_jpa_2024_1.dto;
+
+public class DepartamentoDto {
+
+	private int idDepartamento;
+
+	private String nmDepartamento;
+
+	public DepartamentoDto() {
+
+	}
+
+	public DepartamentoDto(int idDepartamento, String nmDepartamento) {
+		super();
+		this.idDepartamento = idDepartamento;
+		this.nmDepartamento = nmDepartamento;
+	}
+
+	public int getIdDepartamento() {
+		return idDepartamento;
+	}
+
+	public void setIdDepartamento(int idDepartamento) {
+		this.idDepartamento = idDepartamento;
+	}
+
+	public String getNmDepartamento() {
+		return nmDepartamento;
+	}
+
+	public void setNmDepartamento(String nmDepartamento) {
+		this.nmDepartamento = nmDepartamento;
+	}
+
+}
---
+++ src/main/java/com/delegrego/exemplo_jpa_2024_1/dto/FuncionarioDto.java
@@ -0,0 +1,79 @@
+package com.delegrego.exemplo_jpa_2024_1.dto;
+
+public class FuncionarioDto {
+
+	private int idFuncionario;
+
+	private String nome;
+
+	private String email;
+
+	private String senha;
+
+	private double salario;
+
+	private int idDepartamento;
+
+	public FuncionarioDto() {
+
+	}
+
+	public FuncionarioDto(int idFuncionario, String nome, String email, String senha, double salario,
+			int idDepartamento) {
+		this.idFuncionario = idFuncionario;
+		this.nome = nome;
+		this.email = email;
+		this.senha = senha;
+		this.salario = salario;
+		this.idDepartamento = idDepartamento;
+	}
+
+	public int getIdFuncionario() {
+		return idFuncionario;
+	}
+
+	public void setIdFuncionario(int idFuncionario) {
+		this.idFuncionario = idFuncionario;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	public double getSalario() {
+		return salario;
+	}
+
+	public void setSalario(double salario) {
+		this.salario = salario;
+	}
+
+	public int getIdDepartamento() {
+		return idDepartamento;
+	}
+
+	public void setIdDepartamento(int idDepartamento) {
+		this.idDepartamento = idDepartamento;
+	}
+
+}
