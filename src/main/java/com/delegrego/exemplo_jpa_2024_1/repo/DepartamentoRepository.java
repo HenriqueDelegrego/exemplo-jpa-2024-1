88d64dd93046ea08f567ab7ff40e354c112641e9
Henrique Delegrego <62546083+HenriqueDelegrego@users.noreply.github.com>
2025-10-06 21:08:44 -0300

    cria as interfaces de repositório

---
+++ src/main/java/com/delegrego/exemplo_jpa_2024_1/repo/DepartamentoRepository.java
@@ -0,0 +1,9 @@
+package com.delegrego.exemplo_jpa_2024_1.repo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.delegrego.exemplo_jpa_2024_1.entity.DepartamentoEntity;
+
+public interface DepartamentoRepository extends JpaRepository<DepartamentoEntity, Integer> {  
+
+}
