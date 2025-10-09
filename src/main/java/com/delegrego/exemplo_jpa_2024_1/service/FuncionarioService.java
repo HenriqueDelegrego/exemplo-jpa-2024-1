4027c95f23dbd7b8d0697e096aa278c83f0ad2cd
Henrique Delegrego <62546083+HenriqueDelegrego@users.noreply.github.com>
2025-10-08 21:38:06 -0300

    adiciona cadastrarFuncionario

--- src/main/java/com/delegrego/exemplo_jpa_2024_1/service/FuncionarioService.java
+++ src/main/java/com/delegrego/exemplo_jpa_2024_1/service/FuncionarioService.java
@@ -1,5 +1,46 @@
 package com.delegrego.exemplo_jpa_2024_1.service;
 
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.delegrego.exemplo_jpa_2024_1.dto.FuncionarioDto;
+import com.delegrego.exemplo_jpa_2024_1.entity.DepartamentoEntity;
+import com.delegrego.exemplo_jpa_2024_1.entity.FuncionarioEntity;
+import com.delegrego.exemplo_jpa_2024_1.repo.DepartamentoRepository;
+import com.delegrego.exemplo_jpa_2024_1.repo.FuncionarioRepository;
+
+@Service
 public class FuncionarioService {
+
+	@Autowired
+	private FuncionarioRepository funcionarioRepository;
+
+	@Autowired
+	private DepartamentoRepository departamentoRepository;
+
+	// Create
+	public void cadastrarFuncionario(FuncionarioDto funcionarioDto) {
+
+		Optional<DepartamentoEntity> departamentoEntity = departamentoRepository
+				.findById(funcionarioDto.getIdDepartamento());
+
+		FuncionarioEntity funcionarioEntity = new FuncionarioEntity();
+		funcionarioEntity.setNome(funcionarioDto.getNome());
+		funcionarioEntity.setEmail(funcionarioDto.getEmail());
+		funcionarioEntity.setSenha(funcionarioDto.getSenha());
+		funcionarioEntity.setSalario(funcionarioDto.getSalario());
+		funcionarioEntity.setDepartamento(departamentoEntity.get());
+
+		funcionarioRepository.save(funcionarioEntity);
+
+	}
+
+	// Read
+
+	// Update
+
+	// Delete
 
 }
